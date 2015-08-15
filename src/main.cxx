4f03639746984588b3bb640ec90d5d502f7cce25
Ryan Showalter <ryanshow@gmail.com>
2015-08-15 09:11:21 -0500

    Convert tabs to spaces

--- src/main.cxx
+++ src/main.cxx
@@ -14,35 +14,35 @@ static void glfw_key_callback(GLFWwindow *window, const int key, const int scanc
 static void glfw_framebuffer_size_callback(GLFWwindow *window, const int width, const int height);
 
 int main(int argc, char *argv[]) {
-	/*
-	* Initialize GLFW and create a window
-	*/
-	glfwSetErrorCallback(glfw_error_callback);
-	if (!glfwInit()) {
-		exit(EXIT_FAILURE);
-	}
-	GLFWwindow *window = glfwCreateWindow(
-		640, 480,							// width/height
-		fmt::format("EngineMk1 v{}.{}.{}",	// title 
-			EngineMk1_VERSION_MAJOR, 
-			EngineMk1_VERSION_MINOR, 
-			EngineMk1_VERSION_PATCH
-		).c_str(), 
-		NULL,								// windowed mode
-		NULL								// do not share context with another window
-	);
-	if (!window) {
-		glfwTerminate();
-		exit(EXIT_FAILURE);
-	}
-	glfwSetKeyCallback(window, glfw_key_callback);
+    /*
+    * Initialize GLFW and create a window
+    */
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+    GLFWwindow *window = glfwCreateWindow(
+        640, 480,                             // width/height
+        fmt::format("EngineMk1 v{}.{}.{}",    // title 
+            EngineMk1_VERSION_MAJOR, 
+            EngineMk1_VERSION_MINOR, 
+            EngineMk1_VERSION_PATCH
+        ).c_str(), 
+        NULL,                                 // windowed mode
+        NULL                                  // do not share context with another window
+    );
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwSetKeyCallback(window, glfw_key_callback);
     glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
 
-	/*
-	* Initialize glBinding to create the OpenGL context and make it the context for the window
-	*/
-	glbinding::Binding::initialize();
-	glfwMakeContextCurrent(window);
+    /*
+    * Initialize glBinding to create the OpenGL context and make it the context for the window
+    */
+    glbinding::Binding::initialize();
+    glfwMakeContextCurrent(window);
     
     /*
     * Initialize PhysicsFS
@@ -50,36 +50,36 @@ int main(int argc, char *argv[]) {
     PHYSFS_init(argv[0]);
     PHYSFS_setSaneConfig("ryanshow", "EngineMk1", "ZIP", 0, 1);
 
-	/*
-	* Main game loop
-	*/
-	while (!glfwWindowShouldClose(window)) {
-		glClear(GL_COLOR_BUFFER_BIT);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-		//std::this_thread::sleep_for(std::chrono::milliseconds(20));
-	}
+    /*
+    * Main game loop
+    */
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        //std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    }
 
-	/*
-	* Tear down everything
-	*/
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    /*
+    * Tear down everything
+    */
+    glfwDestroyWindow(window);
+    glfwTerminate();
     PHYSFS_deinit();
     
-	return 0;
+    return 0;
 }
 
 static void glfw_error_callback(const int error, const char *description) {
-	fmt::print(stderr, "GLFW ERROR: {}", description);
+    fmt::print(stderr, "GLFW ERROR: {}", description);
 }
 
 static void glfw_key_callback(GLFWwindow *window, const int key, const int scancode, const int action, const int mods) {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-		glfwSetWindowShouldClose(window, true);
-	}
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
 
 static void glfw_framebuffer_size_callback(GLFWwindow *window, const int width, const int height) {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
